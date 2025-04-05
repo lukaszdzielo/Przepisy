a3ab41228a7312ff642ddf07b18921eff0ba2cea
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2025-04-05 12:12:44 +0200

    config update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   app: {
-    baseURL: 'https://lukaszdzielo.github.io/Przepisy/',
+    baseURL: '/Przepisy/',
+    cdnURL: '/Przepisy/'
   },
   modules: [
     '@nuxt/content',
