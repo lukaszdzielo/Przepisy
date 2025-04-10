9cb19d20dc82a332babd6c37090732e6dfb9b96c
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2025-04-10 20:03:36 +0200

    config update

--- content.config.ts
+++ content.config.ts
@@ -3,14 +3,15 @@ import { defineCollection, defineContentConfig, z } from '@nuxt/content'
 export default defineContentConfig({
     collections: {
         recipes: defineCollection({
-            source: '*.md',
             type: 'page',
+            source: '*.md',
             schema: z.object({
                 imgs: z.array(z.string()),
                 time: z.string(),
                 tags: z.array(z.string()),
                 categories: z.array(z.string()),
                 attributes: z.array(z.string()),
+                ebook: z.array(z.string()),
             })
         })
     }
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
 
-const { data: posts } = await useAsyncData('blog', () => queryCollection('recipes')
-    .select('id', 'imgs', 'path', 'title', 'categories', 'time')
-    .all())
+const { data: recipes } = await useAsyncData('blog', () => queryCollection('recipes').all())
 
 </script>
 
 <template>
     <div class="container">
+
         <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-            <NuxtLink v-for="recipe in posts" :key="recipe.id" :to="recipe.path" class="rounded-lg">
+            <NuxtLink v-for="recipe in recipes" :key="recipe.id" :to="recipe.path" class="rounded-lg">
                 <img v-if="recipe.imgs?.length" :src="recipe.imgs[0]" width="300" height="200" loading="lazy"
                     class="aspect-[3/2] object-cover rounded-lg">
                 <div v-else class="w-full aspect-[3/2] bg-slate-100  rounded-lg"></div>
